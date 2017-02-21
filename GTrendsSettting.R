41bf9b70298c7413d78b5cda8956a6eccf40618b
Macron2017 <femmespofr@gmail.com>
2017-02-21 11:17:02 +0100

    Create GTrendsSettting.R

---
+++ GTrendsSettting.R
@@ -0,0 +1,10 @@
+#********************** Install from R
+# install.packages(GTrendsR)
+# 
+#
+#********************** Install from github
+# library(devtools)
+# devtools::install_github("dvanclev/GTrendsR")
+
+## Load library
+library(GTrendsR)
